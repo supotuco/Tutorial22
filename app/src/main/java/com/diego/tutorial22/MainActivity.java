5a831c2003a147766543390a6f5c8755b74205b5
Diego Vela <Diego@Diegos-MacBook-Pro.local>
2016-03-15 15:16:19 -0700

    Initial commit

---
+++ app/src/main/java/com/diego/tutorial22/MainActivity.java
@@ -0,0 +1,26 @@
+package com.diego.tutorial22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+
+}
